206e0a2168f4a82b8a241c9e2c1bfb0b248c20c4
Jackie Johnson-Dallas <j.johnson-dallas@afs.com>
2024-11-09 18:46:51 -0500

    tab, newline, rstrip, lstrip, strip

---
+++ src/whitespace.py
@@ -0,0 +1,15 @@
+# add tab to text using '\t'
+print("Python")
+print("\tPython")
+
+# add newline in a string using '\n'
+print("\nLanguages:\nPython\nC\nJavaScript")
+
+# combine newline and tabs 
+print("\nLanguages:\n\tPython\n\tC\n\tJavaScript")
+
+# strip whitespace using '.strip()' method. you can also remove whitespace from left or right using .lstrip and .rstrip respectively.
+favoriteLanguage = '   python '
+favoriteLanguage = favoriteLanguage.strip()
+print(favoriteLanguage)
+
