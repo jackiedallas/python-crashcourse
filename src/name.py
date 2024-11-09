baffec1431fc680050860b4d4653aa4cdb08bfb3
Jackie Johnson-Dallas <j.johnson-dallas@afs.com>
2024-11-09 18:29:04 -0500

    f strings and upper/lower methods

--- src/name.py
+++ src/name.py
@@ -1,2 +1,10 @@
-name = "ada love lace"
-print(name.title()) # the ".title() capitalizes the first letter of each word in a string."
+# name = "ada love LACE"
+firstname = "aubrey"
+middlename = "drake"
+lastname = "graham"
+fullname = f"{firstname} {middlename} {lastname}"
+# print(name.title()) # the .title() method capitalizes the first letter of each word in a string.
+# print(name.upper()) # the .upper() method capitalizes all letters in the string.
+# print(name.lower()) # the .lower() method lowercases all leters in the string.
+message = f"Hello, {fullname.title()}!"
+print(message)
