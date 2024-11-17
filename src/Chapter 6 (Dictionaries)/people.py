a061c80f3465889f0f30441c818c2fccd01a2888
Jackie Dallas <jackie.r.dallas@gmail.com>
2024-11-17 13:15:01 -0500

    nested dictionary inside dictionary

--- src/Chapter 6 (Dictionaries)/people.py
+++ src/Chapter 6 (Dictionaries)/people.py
@@ -2,36 +2,56 @@ alex = {
     'first_name': 'alex',
     'last_name': 'carter',
     'age': '27',
-    'city': 'new york'
+    'location': {
+        'city': 'new york',
+        'country': 'usa'
+    }
 }
 jordan = {
     'first_name': 'jordan',
     'last_name': 'lee',
     'age': '24',
-    'city': 'london'
+    'location': {
+        'city': 'london',
+        'country': 'united kingdom'
+    }
 }
 taylor = {
     'first_name': 'taylor',
     'last_name': 'morgan',
     'age': '63',
-    'city': 'sydney'
+    'location': {
+        'city': 'sydney',
+        'country': 'australia'
+    }
 }
 casey = {
     'first_name': 'casey',
     'last_name': 'brooks',
     'age': '52',
-    'city': 'toronto'
+    'location': {
+        'city': 'toronto',
+        'country': 'canada'
+    }
 }
 morgan = {
     'first_name': 'morgan',
     'last_name': 'parker',
     'age': '67',
-    'city': 'berlin'
+    'location': {
+        'city': 'berlin',
+        'country': 'germany'
+    }
 }
 
 people = [alex, jordan, taylor, casey, morgan]
 
 for person in people:
     full_name = f"{person['first_name'].title()} {person['last_name'].title()}"
-    location = f"{person['city'].title()}"
-    print(f"{full_name} is {person['age']} years old and lives in {location}.")
+    city = person['location']['city']
+    country = person['location']['country']
+    if country == 'usa':
+        print(f"{full_name} is {person['age']} years old and lives in {city.title()}, {country.upper()}.")
+    else:
+        country.title()
+        print(f"{full_name} is {person['age']} years old and lives in {city.title()}, {country.title()}.")
