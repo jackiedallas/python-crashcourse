525eb14f973e0638ba49bc42790951c766a00f8b
Jackie Dallas <jackie.r.dallas@gmail.com>
2024-12-01 09:11:53 -0500

    added if statement to check for quit

--- src/Chapter 7 (User Inputs and While Loops)/parrot.py
+++ src/Chapter 7 (User Inputs and While Loops)/parrot.py
@@ -10,4 +10,5 @@ prompt += "\nEnter 'quit' to end the program: "
 message = ""
 while message != 'quit':
     message = input(prompt)
-    print(message)
+    if message != 'quit':
+        print(message)
