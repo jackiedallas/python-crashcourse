76a923e675b7c2635642b59f5ef3890bbfdae7b4
Jackie Dallas <jackie.r.dallas@gmail.com>
2024-12-01 09:18:21 -0500

    refactored parrot using a flag

--- src/Chapter 7 (User Inputs and While Loops)/parrot.py
+++ src/Chapter 7 (User Inputs and While Loops)/parrot.py
@@ -7,8 +7,17 @@
 prompt = "\nTell me something and I'll repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program: "
 
-message = ""
-while message != 'quit':
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     if message != 'quit':
+#         print(message)
+
+# create a flag called 'active'
+active = True
+while active:
     message = input(prompt)
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(message)
