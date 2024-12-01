369d2e0615ffa56a3b403a9bca5eefd76de98aac
Jackie Dallas <jackie.r.dallas@gmail.com>
2024-12-01 09:09:09 -0500

    parrot program

--- src/Chapter 7 (User Inputs and While Loops)/parrot.py
+++ src/Chapter 7 (User Inputs and While Loops)/parrot.py
@@ -1,5 +1,13 @@
 # the input function pauses your program and
 # waits for the user to enter some text
 # ask the user to enter some text then display it back to the user
-message = input("How old are you? ")
-print(f"You are {message} years old.")
+# message = input("How old are you? ")
+# print(f"You are {message} years old.")
+
+prompt = "\nTell me something and I'll repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program: "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
