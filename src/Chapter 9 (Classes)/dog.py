7f45849b2956ee2016a4c9ee5701de090e132cf8
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-01-01 22:57:29 -0500

    called methods associated with class

--- src/Chapter 9 (Classes)/dog.py
+++ src/Chapter 9 (Classes)/dog.py
@@ -18,3 +18,7 @@ class Dog:
 my_dog = Dog('Bandit', 12)
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
+
+# Call the methods associated with the Dog class.
+my_dog.sit()
+my_dog.roll_over()
