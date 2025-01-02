0cc355fe0d61035a7ce5434c5a6751acb44d941f
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-01-01 22:42:04 -0500

    working with different import methods

--- src/Chapter 8 (Functions)/making_pizzas.py
+++ src/Chapter 8 (Functions)/making_pizzas.py
@@ -1,4 +1,13 @@
 # from ..modules import pizza
-import pizza
+# import pizza
+# from pizza import make_pizza
+# from pizza import make_pizza as mp
+# import pizza as p
+from pizza import *
 
-pizza.make_pizza(16, 'pepperoni', 'extra cheese', 'olives', 'sausage')
+# pizza.make_pizza(16, 'pepperoni', 'extra cheese', 'olives', 'sausage')
+# pizza.make_pizza(12, 'pineapples', 'alfredo sauce')
+
+make_pizza(24, 'sausage', 'extra cheese')
+# mp(18, 'pepperoni', 'extra cheese', 'stuffed crust')
+# p.make_pizza(12, 'cheese', 'sausage')
