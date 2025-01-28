c8aedfc58ba4d2fd76cf7c826fcd9e9cd1c101bd
Jackie Johnson-Dallas <j.johnson-dallas@afs.com>
2025-01-28 12:05:15 -0500

    added doc strings

--- src/Chapter 10 (Files and Exceptions)/fav_number.py
+++ src/Chapter 10 (Files and Exceptions)/fav_number.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import json
 
 def new_fav_number(file_path):
+    """Prompt user for a new favorite number and save it at the file path."""
     path = Path(file_path)
     num = input("What is your favorite number? ")
     contents = json.dumps(num)
@@ -9,6 +10,7 @@ def new_fav_number(file_path):
     print(f"We'll save your favorite number til you get back!")
 
 def get_fav_number(file_path):
+    """Check if the file path exists and return the contents"""
     path = Path(file_path)
     if path.exists():
         contents = path.read_text()
@@ -16,6 +18,10 @@ def get_fav_number(file_path):
         return num
         
 def fav_number(file_path):
+    """
+    Use the functions defined above to recall a 
+    favorite number or store a new one.
+    """
     path = Path(file_path)
     number = get_fav_number(path)
     if number:
