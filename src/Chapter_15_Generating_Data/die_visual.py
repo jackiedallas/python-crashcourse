f42e7d3a5d88f426395e8d4facd1d4cb2ac5c831
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-02-09 10:06:09 -0500

    added the results of rolling two die and used the update layout method to label all possible results

--- src/Chapter_15_Generating_Data/die_visual.py
+++ src/Chapter_15_Generating_Data/die_visual.py
@@ -10,29 +10,39 @@ die_2 = Die()
 results = []
 
 for roll_num in range(1000):
-    rolls = die_1.roll()
+    rolls = die_1.roll() + die_2.roll()
     results.append(rolls)
     
 # analyze the results
 frequencies = []
-possible_results = range(1, die.num_sides+1)
+# possible_results = range(1, die.num_sides+1)
+max_results = die_1.num_sides + die_2.num_sides
+possible_results = range(2, max_results+1)
 for value in possible_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
+print(len(frequencies))
+print(frequencies)
 print(f"""
 Times Rolled:
 
-1: {frequencies[0]}
-2: {frequencies[1]}
-3: {frequencies[2]}
-4: {frequencies[3]}
-5: {frequencies[4]}
-6: {frequencies[5]}
+2: {frequencies[0]}
+3: {frequencies[1]}
+4: {frequencies[2]}
+5: {frequencies[3]}
+6: {frequencies[4]}
+7: {frequencies[5]}
+8: {frequencies[6]}
+9: {frequencies[7]}
+10: {frequencies[8]}
+11: {frequencies[9]}
+12: {frequencies[10]}
 """)
 
 # visualize the results
-title = "Results of Rolling One D6 1,000 Times"
+title = "Results of Rolling Two D6 1,000 Times"
 labels = {'x': 'Results', 'y': 'Frequency of Results'}
 fig = px.bar(x=possible_results, y=frequencies, title=title, labels=labels)
+fig.update_layout(xaxis_dtick=1)
 fig.show()
